f035b589ee450844f390881f82434f597d98165d
= <=>
2019-07-04 15:56:00 +0700

    PT Bac 1

---
+++ src/PTBac_1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PTBac_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap a: ");
+        double a = scanner.nextDouble();
+        System.out.print("Nhap b: ");
+        double b = scanner.nextDouble();
+        System.out.println("Phuong trinh bac nhat da nhap la y= " + a + "x " + b + " = 0");
+        if (b == 0) {
+            System.out.print("x = "+0);
+        }
+        if (b != 0){
+            System.out.print("x = "+-b/a);
+        }
+    }
+}
